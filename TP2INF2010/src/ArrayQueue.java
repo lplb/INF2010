aa9170e5b5a35b1803ab3ee63b44fc71f97e504c
Client <Client@DESKTOP-JHS00ET>
2017-02-08 10:22:00 -0500

    Commit d'ArrayQueue et de lisp, ArrayQueue est complet

--- TP2INF2010/src/ArrayQueue.java
+++ TP2INF2010/src/ArrayQueue.java
@@ -1,60 +1,70 @@
-
-
-public class ArrayQueue<AnyType> implements Queue<AnyType>
-{
-	private int size = 0;		//Nombre d'elements dans la file.
-	private int startindex = 0;	//Index du premier element de la file
+public class ArrayQueue<AnyType extends Object> implements Queue<AnyType> {
+	private final int INIT_SIZE = 10;
+	private int size = 0; // Nombre d'elements dans la file.
+	private int startIndex = 0; // Index du premier element de la file
 	private AnyType[] table;
-   
-	@SuppressWarnings("unchecked")
-	public ArrayQueue() 
-	{
-		//A completer
-	}
-	
-	//Indique si la file est vide
-	public boolean empty() 
-	{ 
-		return size == 0; 
-	}
-	
-	//Retourne la taille de la file
-	public int size() 
-	{ 
-		return size; 
-	}
-	
-	//Retourne l'element en tete de file
-	//Retourne null si la file est vide
-	//complexité asymptotique: O(1)
-	public AnyType peek()
-	{
-		//A completer
-	}
-	
-	//Retire l'element en tete de file
-	//complexité asymptotique: O(1)
-	public void pop() throws EmptyQueueException
-	{
-		//A completer
-	}
-	
-	//Ajoute un element a la fin de la file
-	//Double la taille de la file si necessaire
-	//complexité asymptotique: O(1) ( O(N) lorsqu'un redimensionnement est necessaire )
-	public void push(AnyType item)
-	{
-		//A completer
-	}
-   
-	//Redimensionne la file. La capacite est multipliee par un facteur de resizeFactor.
-	//Replace les elements de la file au debut du tableau
-	//complexité asymptotique: O(N)
-	@SuppressWarnings("unchecked")
-	private void resize(int resizeFactor)
-	{
-		//A completer
 
-	}   
+	@SuppressWarnings("unchecked")
+	public ArrayQueue() {
+		table = (AnyType[]) new Object[INIT_SIZE];
+	}
+
+	// Indique si la file est vide
+	public boolean empty() {
+		return size == 0;
+	}
+
+	// Retourne la taille de la file
+	public int size() {
+		return size;
+	}
+
+	// Retourne l'element en tete de file
+	// Retourne null si la file est vide
+	// complexité asymptotique: O(1)
+	public AnyType peek() {
+		if(!this.empty())
+			return table[startIndex];
+		else
+			return null;
+	}
+
+	// Retire l'element en tete de file
+	// complexité asymptotique: O(1)
+	public void pop() throws EmptyQueueException {
+		if(!this.empty()){
+			startIndex++;
+			size--;
+		}
+	}
+
+	// Ajoute un element a la fin de la file
+	// Double la taille de la file si necessaire
+	// complexité asymptotique: O(1) ( O(N) lorsqu'un redimensionnement est
+	// necessaire )
+	public void push(AnyType item) {
+		try{
+			table[size+startIndex] = item;
+			size++;
+		} catch(ArrayIndexOutOfBoundsException e){
+			resize(2);
+			table[size+startIndex] = item;
+			size++;
+		}
+	}
+
+	// Redimensionne la file. La capacite est multipliee par un facteur de
+	// resizeFactor.
+	// Replace les elements de la file au debut du tableau
+	// complexité asymptotique: O(N)
+	@SuppressWarnings("unchecked")
+	private void resize(int resizeFactor) {
+		AnyType[] temp = (AnyType[]) new Object[size*resizeFactor + 1];
+		for(int i = startIndex, j = 0; i<size+startIndex; i++, j++){
+			temp[i] = table[j];
+		}
+		table = temp;
+		startIndex = 0;
+
+	}
 }
-
--- TP2INF2010/src/Lisp.java
+++ TP2INF2010/src/Lisp.java
@@ -4,68 +4,87 @@ import java.util.Vector;
 
 public class Lisp {
 
-	
-/*
- * cette fonction permet de resoudre  une expresion Lisp   
- * le paramètre peut être transformer en token à l'aide de la fonction getTokens(expresion) 
- * NB une seule pile peut être utilisée
- * retourn "double" le resultat de l'expresion 
-*/
-static public  double solve(String expresion){
-		Stack<String> stack = new Stack<String>();
-		//A complete
-	
-}			
-/*
- * cette fonction vérifier si une expression est équilibree 
- * i.e. toutes parenthèse ouverte à une parenthèse fermante
- * N.B: une seule pile peut être utilisée 
- * return true si equilibree, false sionon
- */
 
-static public boolean isEquilibre(String expresion){		
-	Stack<String> stack = new Stack<String>();
-	//A completer 
-}
+	/*
+	 * cette fonction permet de resoudre  une expresion Lisp   
+	 * le paramètre peut être transformer en token à l'aide de la fonction getTokens(expresion) 
+	 * NB une seule pile peut être utilisée
+	 * retourn "double" le resultat de l'expresion 
+	 */
+	static public  double solve(String expression){
+		Stack<Character> stack = new Stack<Character>();
+		for(int i = 0; i<expression.length();i++)
+			stack.push(expression.charAt(i));
+		switch(stack.pop()){
 
-/*
- * fonction transforme une expresion (String) lisp en tokens (Vector<String>)
- */
-static public Vector<String> getTokens(String expresion){
-	
-	Vector<String> vectorOfTokens=new Vector<String>();
-	int lenght=expresion.length();
-	String token="",number="";
-	//==
-	for(int i=lenght-1;i>=0;i--) {
-		//
-		token=String.valueOf(expresion.charAt(i));
-		if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
-			if(!number.isEmpty()){
-				vectorOfTokens.addElement(number);
-				number="";	
-			}
-			vectorOfTokens.addElement(token);
-		}else if(token.equals(")")|| token.equals("(") ){
-			if(!number.isEmpty()){
-				vectorOfTokens.addElement(number);
-				number="";	
-			}	
-			vectorOfTokens.addElement(token);				
-		}else if(token.equals(" ")){
-			if(!number.isEmpty()){
-				vectorOfTokens.addElement(number);
-				number="";	
-			}	
-		}else if(!token.equals(" ")){
-			number=token+number;		
 		}
+
+	}			
+	/*
+	 * cette fonction vérifier si une expression est équilibree 
+	 * i.e. toutes parenthèse ouverte à une parenthèse fermante
+	 * N.B: une seule pile peut être utilisée 
+	 * return true si equilibree, false sionon
+	 */
+
+	static public boolean isEquilibre(String expression){		
+		//Stack<String> stack = new Stack<String>();
+		int amountLeft = 0, amountRight = 0;
+
+		for(int i = 0; i<expression.length();i++){
+			if(expression.charAt(i) == '(')
+				amountRight++;
+			if(expression.charAt(i) == ')')
+				amountLeft++;
+		}
+
+		return amountRight == amountLeft;
+
+		//	for(int i = 0; i<expression.length();i++)
+		//		stack.push(expression.substring(i, i+1));
+		//	while(!stack.isEmpty()){
+		//		
+		//	}
 	}
-	// invert vectorOfTokens;
-	 Collections.reverse(vectorOfTokens);
-	
-	return vectorOfTokens;
-}
+
+	/*
+	 * fonction transforme une expresion (String) lisp en tokens (Vector<String>)
+	 */
+	static public Vector<String> getTokens(String expression){
+
+		Vector<String> vectorOfTokens=new Vector<String>();
+		int lenght=expression.length();
+		String token="",number="";
+		//==
+		for(int i=lenght-1;i>=0;i--) {
+			//
+			token=String.valueOf(expression.charAt(i));
+			if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
+				if(!number.isEmpty()){
+					vectorOfTokens.addElement(number);
+					number="";	
+				}
+				vectorOfTokens.addElement(token);
+			}else if(token.equals(")")|| token.equals("(") ){
+				if(!number.isEmpty()){
+					vectorOfTokens.addElement(number);
+					number="";	
+				}	
+				vectorOfTokens.addElement(token);				
+			}else if(token.equals(" ")){
+				if(!number.isEmpty()){
+					vectorOfTokens.addElement(number);
+					number="";	
+				}	
+			}else if(!token.equals(" ")){
+				number=token+number;		
+			}
+		}
+		// invert vectorOfTokens;
+		Collections.reverse(vectorOfTokens);
+
+		return vectorOfTokens;
+	}
 
 
 }
